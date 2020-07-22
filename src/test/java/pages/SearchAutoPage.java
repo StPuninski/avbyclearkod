46c0850831baf7865bbeffcd7f4b760756022d3c
StPuninski <stpuninski@gmail.com>
2020-07-22 19:27:17 +0300

    do not work

--- src/test/java/pages/SearchAutoPage.java
+++ src/test/java/pages/SearchAutoPage.java
@@ -8,7 +8,6 @@ public abstract class SearchAutoPage extends BasePage {
     private static final String URL = "https://cars.av.by/search/extended?brand_id%5B%5D=&model_id%5B%5D=&year_from=&year_to=&currency=USD&price_from=&price_to=";
     private static final By SEARCH_BUTTON = By.cssSelector(".button button-primary");
 
-
     public SearchAutoPage(WebDriver driver) {
         super(driver);
     }
